677813fda2c8111dff4ff737f843c76d36cb1b6d
Alessandro Galasso <alessandro.galasso@eurac.edu>
2020-07-15 09:01:19 +0200

    Added  parameter to lang.ps1.

--- lang.ps1
+++ lang.ps1
@@ -4,14 +4,39 @@
 # Requires: Docker Desktop.
 #
 
-# swagger-codegen
-# https://swagger.io/tools/swagger-codegen/
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli langs
-$lang = Read-Host 'Type the language you need'
-docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli config-help -l $lang
+param (
+    [Parameter()]
+    [ValidateSet('swagger-codegen','openapi-generator')]
+    $framework='swagger-codegen'
+)
 
-# openapi-generator
-# https://openapi-generator.tech/
-#docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli list
-#$lang = Read-Host 'Type the language you need'
-#docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli config-help -g $lang
+$title = "
+Available languages and settings
+================================
+Framework:       '$framework'
+
+"
+
+Write-Host $title
+
+if ($framework -eq 'swagger-codegen') {
+
+    # swagger-codegen
+    # https://swagger.io/tools/swagger-codegen/
+    docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli langs
+    $lang = Read-Host 'Type the language you need'
+    docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli config-help -l $lang
+
+} elseif ($framework -eq 'openapi-generator') {
+
+    # openapi-generator
+    # https://openapi-generator.tech/
+    docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli list
+    $lang = Read-Host 'Type the language you need'
+    docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli config-help -g $lang
+
+} else {
+
+    Write-Host 'Not supported framework.'
+
+}
